2ded8add66cf299984e463e43f431830a503b730
ashikask2002 <ksashik999@gmail.com>
2024-02-24 14:19:24 +0530

    added addaddress testcase

--- pkg/usecase/user_test.go
+++ pkg/usecase/user_test.go
@@ -7,6 +7,7 @@ import (
 	"GlassGalore/pkg/domain"
 	mockhelper "GlassGalore/pkg/helper/mock"
 	mockrepo "GlassGalore/pkg/repository/mock"
+	"GlassGalore/pkg/utils/models"
 
 	"github.com/go-playground/assert/v2"
 	"github.com/golang/mock/gomock"
@@ -52,3 +53,55 @@ func Test_GetAddresses(t *testing.T) {
 	}
 }
 
+func Test_AddAddress(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mockrepo.NewMockUserRepository(ctrl)
+	helper := mockhelper.NewMockHelper(ctrl)
+
+	userUsecase := NewUserUseCase(userRepo, helper)
+	testData := map[string]struct {
+		input   models.AddAddress
+		stub    func(*mockrepo.MockUserRepository, *mockhelper.MockHelper, models.AddAddress)
+		WantErr error
+	}{
+		"success": {
+			input: models.AddAddress{
+				Name:      "ashik",
+				HouseName: "kizhakoodan",
+				Street:    "kanakamala",
+				City:      "chalakudy",
+				State:     "kerala",
+				Phone:     "7510468624",
+				Pin:       "680680",
+			},
+			stub: func(userrepo *mockrepo.MockUserRepository, helper *mockhelper.MockHelper, data models.AddAddress) {
+				userRepo.EXPECT().CheckIfFirstAddress(1).Times(1).Return(false) // Setting expectation for CheckIfFirstAddress
+				userRepo.EXPECT().AddAddress(1, data, true).Times(1).Return(nil)
+			},
+			WantErr: nil,
+		},
+		"failure": {
+			input: models.AddAddress{
+				Name:      "ashik",
+				HouseName: "kizhakoodan",
+				Street:    "kanakamala",
+				City:      "chalakudy",
+				State:     "kerala",
+				Phone:     "7510468624",
+				Pin:       "680680",
+			},
+			stub: func(userrepo *mockrepo.MockUserRepository, helper *mockhelper.MockHelper, data models.AddAddress) {
+				userRepo.EXPECT().CheckIfFirstAddress(1).Times(1).Return(false) // Setting expectation for CheckIfFirstAddress
+				userRepo.EXPECT().AddAddress(1, data, true).Times(1).Return(errors.New("error in adding"))
+			},
+			WantErr: errors.New("error in adding address"),
+		},
+	}
+	for _, test := range testData {
+		test.stub(userRepo, helper, test.input)
+		err := userUsecase.AddAddress(1, test.input)
+		assert.Equal(t, test.WantErr, err)
+	}
+}
