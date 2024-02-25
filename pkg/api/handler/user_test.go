118730a82a6a5ab87a6836554eddf35aad1d3775
ashikask2002 <ksashik999@gmail.com>
2024-02-25 17:57:13 +0530

    added signup in handler testcase

--- pkg/api/handler/user_test.go
+++ pkg/api/handler/user_test.go
@@ -1,3 +1,97 @@
 package handler
 
+import (
+	"GlassGalore/pkg/utils/models"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
+	mock_interfaces "GlassGalore/pkg/usecase/mock"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_UserSignUp(t *testing.T) {
+	testcase := map[string]struct {
+		input         models.UserDetails
+		buildstub     func(useCaseMock *mock_interfaces.MockUserUseCase, sighupData models.UserDetails)
+		checkResponse func(t *testing.T, responseRecorder *httptest.ResponseRecorder)
+	}{
+		"valid Signup": {
+			input: models.UserDetails{
+				Name:            "ashik",
+				Email:           "ashik@gmail.com",
+				Phone:           "7510468623",
+				Password:        "12345",
+				ConfirmPassword: "12345",
+			},
+			buildstub: func(useCaseMock *mock_interfaces.MockUserUseCase, sighupData models.UserDetails) {
+				err := validator.New().Struct(sighupData)
+				if err != nil {
+					fmt.Println("validation failed")
+				}
+				useCaseMock.EXPECT().UserSignUp(sighupData).Times(1).Return(models.TokenUsers{
+					Users: models.UserDetailsResponse{
+						Id:    1,
+						Name:  "ashik",
+						Email: "ashik@gmail.com",
+						Phone: "7510468623",
+					},
+					Token: "aksjgnal.fiugliagbldfgbldf.gdbladfjnb",
+				}, nil)
+			},
+			checkResponse: func(t *testing.T, responseRecorder *httptest.ResponseRecorder) {
+				assert.Equal(t, http.StatusCreated, responseRecorder.Code)
+			},
+		},
+		"user could not sign up": {
+			input: models.UserDetails{
+				Name:            "ashik",
+				Email:           "ashik@gmail.com",
+				Phone:           "7510468623",
+				Password:        "12345",
+				ConfirmPassword: "12345",
+			},
+			buildstub: func(useCaseMock *mock_interfaces.MockUserUseCase, signupData models.UserDetails) {
+				err := validator.New().Struct(signupData)
+				if err != nil {
+					fmt.Println("vallidation failed")
+				}
+				useCaseMock.EXPECT().UserSignUp(signupData, ).Times(1).Return(models.TokenUsers{}, errors.New("cannot signup"))
+			},
+			checkResponse: func(t *testing.T, responseRecorder *httptest.ResponseRecorder) {
+				assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+			},
+		},
+	}
+	for testName, test := range testcase {
+		test := test
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+			ctrl := gomock.NewController(t)
+			mockUseCase := mock_interfaces.NewMockUserUseCase(ctrl)
+			test.buildstub(mockUseCase, test.input)
+			UserHandler := NewUserHandler(mockUseCase)
+
+			server := gin.Default()
+			server.POST("/signup", UserHandler.UserSignUp)
+
+			jsonData, err := json.Marshal(test.input)
+			assert.NoError(t,err)
+			body := bytes.NewBuffer(jsonData)
+
+			mockRequest, err := http.NewRequest(http.MethodPost, "/signup",body)
+			assert.NoError(t, err)
+			responseRecorder := httptest.NewRecorder()
+			server.ServeHTTP(responseRecorder,mockRequest)
+			test.checkResponse(t, responseRecorder )
+		})
+	}
+}
